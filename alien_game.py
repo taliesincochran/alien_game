c359716ecd2d77e62e9e41f4530fc4e11031fa93
T. David Cochran <tdcochran2@gmail.com>
2021-07-12 20:03:37 -0400

    added rows of aliens

--- alien_game.py
+++ alien_game.py
@@ -98,18 +98,24 @@ class AlienGame:
     def _create_fleet(self):
         """"Create a fleet of alien ships"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        
+        #Determine the number of rows
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_of_rows = available_space_y // (2* alien_height)
+        for row_number in range(number_of_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
             
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width +2 * alien_width * alien_number
         alien.rect.x = alien.x
-        self.aliens.add(alien)
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         # create the first row of aliens
         self.aliens.add(alien)
     
